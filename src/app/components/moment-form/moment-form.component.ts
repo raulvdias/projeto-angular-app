5b3c6409861f03758503e73a109854040bfe0ad1
Raul Victor <raul_souzadias@hotmail.com>
2022-05-24 13:06:24 -0300

    Adicionando função de envio de Form

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,4 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { EventEmitter } from '@angular/core';
+
+import { Moment } from 'src/app/Moment';
 
 @Component({
   selector: 'app-moment-form',
@@ -7,9 +11,40 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class MomentFormComponent implements OnInit {
   @Input() btnText!: String;
+  @Output() onSubmit = new EventEmitter<Moment>();
+
+  momentForm!: FormGroup;
+  
   constructor() { }
 
   ngOnInit(): void {
+    this.momentForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      image: new FormControl(''),
+    })
   }
 
+  get title(){
+    return this.momentForm.get('title')!;
+  }
+
+  get description(){
+    return this.momentForm.get('description')!;
+  }
+
+  onFileSelected(event:any){
+    const file: File = event.target.files[0];
+    this.momentForm.patchValue({image: file})
+  }
+
+  submit(){
+
+    if(this.momentForm.invalid){
+      return;
+    }
+    console.log(this.momentForm.value);
+    this.onSubmit.emit(this.momentForm.value)
+  }
 }
--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Moment } from 'src/app/Moment';
+import { MomentService } from 'src/app/services/moment.service';
+
+
 
 @Component({
   selector: 'app-new-moment',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewMomentComponent implements OnInit {
   btnText: string = 'Compartilhar!';
-  constructor() { }
+  constructor(private momentService: MomentService) { }
 
   ngOnInit(): void {
   }
 
+  async createHandler(moment: Moment){
+    const formData = new FormData();
+
+    formData.append('title', moment.title);
+    formData.append('description', moment.description);
+    
+    if(moment.image){
+      formData.append('image', moment.image);
+    }
+
+    //todo
+
+    await this.momentService.createMoment(formData).subscribe();
+
+    //exibir msg
+
+    //redirect
+  }
 }
